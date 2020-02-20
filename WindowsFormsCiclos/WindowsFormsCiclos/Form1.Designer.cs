23261079e4fb90883dc57838af604406fb5ad5f1
Reginaldo Barros <reginaldo.barros@icloud.com>
2020-02-20 01:04:42 -0300

    Segundo Commit

---
+++ WindowsFormsCiclos/WindowsFormsCiclos/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsCiclos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmd_Executar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cmd_Executar
+            // 
+            this.cmd_Executar.Location = new System.Drawing.Point(670, 396);
+            this.cmd_Executar.Name = "cmd_Executar";
+            this.cmd_Executar.Size = new System.Drawing.Size(75, 23);
+            this.cmd_Executar.TabIndex = 0;
+            this.cmd_Executar.Text = "Executar";
+            this.cmd_Executar.UseVisualStyleBackColor = true;
+            this.cmd_Executar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 148);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 290);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.cmd_Executar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmd_Executar;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsCiclos/WindowsFormsCiclos/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsCiclos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //For
+
+            //for (int valor = 0; valor <= 100; valor++)
+            //{ 
+            //    listBox1.Items.Add(valor);
+            //}
+
+            //int vezes = 0;
+            //while (vezes <100)
+
+            //{
+            //    listBox1.Items.Add(vezes);
+            //    vezes++;
+            //}
+
+            //listBox1.Items.Clear();
+
+            //int vezes = 0;
+            //do
+            //{
+            //    listBox1.Items.Add(vezes);
+            //    vezes++;
+            //} while (vezes <=100);
+
+            string frase = "Este Ciclo é fantastico";
+
+            //listBox1.Items.Add(frase);
+            //listBox1.Items.Add(frase.ToUpper());
+            //listBox1.Items.Add(frase.Length);
+
+            List<string> lista_nome = new List<string>()
+            {
+                "Joao","Antonio","Franscisco","Carlos","Ana","Patricia","Ana"
+            };
+
+            foreach (string s in lista_nome)
+            {
+                listBox1.Items.Add(s);
+            }
+            //foreach (char c in frase)
+                            
+            //{
+            //    listBox1.Items.Add(c);
+            //}
+
+            }
+        }
+    
+}
